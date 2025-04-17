3fc469d9548b54ad039a8d28398762159477d54c
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-04-16 21:27:57 -0300

    Adicionando contatos

--- AgendaTelefonica/src/AgendaTelefonica.java
+++ AgendaTelefonica/src/AgendaTelefonica.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AgendaTelefonica {
@@ -6,22 +5,59 @@ public class AgendaTelefonica {
 
         boolean estaAtivo = true;
         String entrada;
-
         Scanner s = new Scanner(System.in);
-        ArrayList<String> agendaTelefonica = new ArrayList<>();
-
+        String[][] listaTelefonica = new String[100][2];
+        int totalContatos = 0;
         while (estaAtivo) {
             System.out.println("Digite o que deseja:\n" +
-                    "1 - Adicionar novo contato\n" +
+                    "1 - Adicionar contato\n" +
                     "2 - Buscar contato\n" +
                     "3 - Lista de contatos\n" +
-                    "4 - Remover contato\n" +
-                    "x - sair");
+                    "4 - Remover contatos\n" +
+                    "x - Sair");
             entrada = s.nextLine();
+            switch (entrada) {
+                case "1":
 
-            boolean querContinuar = true;
+                    boolean querContinuar = true;
+                    while (querContinuar && totalContatos < listaTelefonica.length) {
+                        System.out.println("Digite o nome do contato:");
+                        String nome = s.nextLine();
+
+                        System.out.println("Digite o telefone:");
+                        String telefone = s.nextLine();
 
 
+                        listaTelefonica[totalContatos][0] = nome;
+                        listaTelefonica[totalContatos][1] = telefone;
+                        totalContatos++;
+
+                        System.out.println("Contato adicionado com sucesso!");
+
+                        System.out.println("Deseja adicionar outro contato? (s/n)");
+                        String resposta = s.nextLine();
+
+                        if (resposta.equalsIgnoreCase("n")) {
+                            querContinuar = false;
+                        }
+                    }
+
+                    if (totalContatos == listaTelefonica.length) {
+                        System.out.println("A agenda está cheia.");
+                    }
+
+                    break;
+
+                case "x":
+                    estaAtivo = false;
+                    break;
+
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+            }
         }
+
+        System.out.println("Você saiu do programa.");
+        s.close();
     }
 }
